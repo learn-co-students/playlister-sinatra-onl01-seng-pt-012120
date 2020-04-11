340639c74957a1789f9c5baaa778cbdf9166b57b
d-otis <dan@dan-foley.com>
2020-04-11 15:34:35 -0400

    Done.

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,7 +1,8 @@
 class Genre < ActiveRecord::Base
 
-	has_many :songs
-	has_many :artists, through: :song_genre
+	has_many :song_genres
+	has_many :songs, through: :song_genres
+	has_many :artists, through: :songs
 	
 
 end
