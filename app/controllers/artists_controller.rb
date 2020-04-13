e0b6e871c78d9d51a3989979dca22216eafe671f
d-otis <dan@dan-foley.com>
2020-04-13 14:36:18 -0400

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -9,6 +9,7 @@ class ArtistsController < ApplicationController
 	get '/artists/:slug' do
 		@artist = Artist.find_by_slug(params[:slug])
 		@genres = @artist.genres
+		binding.pry
 
 		erb :'/artists/show'
 	end
